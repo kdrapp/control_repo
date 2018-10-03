d35693c9a92c3818c1f638f2b844c04e44393ffc
kdrapp <ken.r.drapp@rrd.com>
2018-10-03 14:33:17 -0500

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -1,15 +1,15 @@
 class profile::ssh_server {
-	package {'openssh-server':
-		ensure => present,
-	}
-	service { 'sshd':
-		ensure => 'running',
-		enable => 'true',
-	}
-	ssh_authorized_key { 'root@master.puppet.vm':
-		ensure => present,
-		user   => 'root',
-		type   => 'ssh-rsa',
-		key    => '',
-	}  
+        package {'openssh-server':
+                ensure => present,
+        }
+        service { 'sshd':
+                ensure => 'running',
+                enable => 'true',
+        }
+        ssh_authorized_key { 'root@master.puppet.vm':
+                ensure => present,
+                user   => 'root',
+                type   => 'ssh-rsa',
+                key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQCsGsAdjF+f+mPdMrcgfzVy9N0ExPP2M0d4VN4Gk5mgqk3UaRpxE0o7xL2Ct1fNE/QLab9ewpjcpZxf/eNT0v6I2Cu2DJR7ZIrsAAtCRPrkbQMl1HGpp3xxekGHT3Wh/IUNg5iuemxC2xDl5FTSmrEEe7GYP4GdMJC/P9LQg6AwHqn2FME3ce+o2WusqvwoG9UCFV34nw0vc6jvcJ9u475ExZtY04z5GazcCFVlQVCZ+43ViGkkhndRHrMVBLYXLUeRCfQm3P8e9k+ePdQqNxh7wvVAu3eAf86SM812DBmypnuThNwFm692sVhMFXGBpKrFw4q58wEATkBMI6n8nIdV',
+        }  
 }
